11d206dad9540cf50309b173eb69c7440d5d0982
Fahad Kazi <fhkz2005@gmail.com>
2016-06-13 10:58:30 +0530

    first commit

---
+++ app/routes.js
@@ -0,0 +1,23 @@
+// app/routes.js
+
+module.exports = function(app) {
+
+    // server routes ===========================================================
+    // handle things like api calls
+    // authentication routes
+
+    // sample api route
+    app.get('/', function(req, res) {
+        res.json({ message: 'hooray! welcome to our api!' });
+    });
+
+    // route to handle creating goes here (app.post)
+    // route to handle delete goes here (app.delete)
+
+    // frontend routes =========================================================
+    // route to handle all angular requests
+    app.get('*', function(req, res) {
+        res.sendfile('./public/views/index.html'); // load our public/index.html file
+    });
+
+};
---
+++ config/db.js
@@ -0,0 +1,4 @@
+// config/db.js
+module.exports = {
+    url : 'mongodb://localhost/your-database' // Replace your-database with your real database name
+}
